793ec07b9b62bacf402a1dc057331f55232d9be1
KhalidAltheeb <w.altheeb@gmail.com>
2021-02-05 01:50:28 +0300

    refactor: Display day_name

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -65,7 +65,7 @@ def load_data(city, month, day):
 
     # extract month and day of week from Start Time to create new columns
     df['month'] = df['Start Time'].dt.month
-    df['day_of_week'] = df['Start Time'].dt.day_name()
+    df['day_name'] = df['Start Time'].dt.day_name()
 
 
     # filter by month if applicable
@@ -80,7 +80,7 @@ def load_data(city, month, day):
     # filter by day of week if applicable
     if day != 'all':
         # filter by day of week to create the new dataframe
-        df = df[df['day_of_week'] == day.title()]
+        df = df[df['day_name'] == day.title()]
 
     return df
 
@@ -95,7 +95,7 @@ def time_stats(df):
     print('The most common month is: ', df['month'].mode()[0], '\n')
 
     # TO DO: display the most common day of week
-    print('The most commen day of the week is: ', df['day_of_week'].mode()[0], '\n')
+    print('The most commen day of the week is: ', df['day_name'].mode()[0], '\n')
 
     # TO DO: display the most common start hour
     df['hour'] = df['Start Time'].dt.hour
